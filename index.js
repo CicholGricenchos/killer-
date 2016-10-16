c52c1f35f72ec9cbb9ad1a8a422d7660e56d5a01
Cichol <cichol@live.cn>
2016-10-16 11:01:11 +0800

    add eachFrame

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-exports.sheerHeartAttack = function(){
-	Object.defineProperty(Object.prototype, 'kq', {
-		value: function(func, ...args){
-			return func.call(this, this, ...args)
-		}
-	})
+var killerqueen = {
+	sheerHeartAttack(){
+		Object.defineProperty(Object.prototype, 'kq', {
+			value: function(func, ...args){
+				return func.call(this, this, ...args)
+			}
+		})
 
-	Object.defineProperty(Function.prototype, 'new', {
-		value: function(...args){
-			return new this(...args)
+		Object.defineProperty(Function.prototype, 'new', {
+			value: function(...args){
+				return new this(...args)
+			}
+		})
+
+		Promise.immediately = function(){
+			return new Promise(resolve => resolve())
 		}
-	})
+	},
+
+	eachFrame(callback){
+		var handler = function(){
+			if(callback() != false){
+				requestAnimationFrame(handler)
+			}
+		}
+		requestAnimationFrame(handler)
+	}
 }
+
+module.exports = killerqueen
